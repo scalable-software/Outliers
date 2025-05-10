1db120c2e39a51bb8949b912209e550fc8aa39b1
Architect <solution.architect@scalable.software>
2025-05-10 18:53:38 +0200

    EXCEPTIONS: Scaling.Exceptions -> PASS

--- R/Scaling.Exceptions.R
+++ R/Scaling.Exceptions.R
@@ -11,7 +11,7 @@
 Scaling.Exceptions <- \() {
   exceptions <- list()
   exceptions[['argument.NULL']] <- \(invoke) {
-    if(invoke) stop("Argument.NULL: 'sample' cannot be NULL.", call. = FALSE)
+    if(invoke) stop("Argument.NULL: 'medcouple' cannot be NULL.", call. = FALSE)
   }
   exceptions[['type.mismatch']] <- \(invoke, actual = NULL) {
     if (invoke) stop("Type.Mismatch: Got '", actual, "' but expected 'numeric'.", call. = FALSE)
