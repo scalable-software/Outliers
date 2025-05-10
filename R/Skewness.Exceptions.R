1b83bbd60c0b830871073b3ed2569e61bdd934e7
Architect <solution.architect@scalable.software>
2025-05-10 12:42:07 +0200

    EXCEPTIONS: Skewness.Exceptions -> PASS

---
+++ R/Skewness.Exceptions.R
@@ -0,0 +1,20 @@
+#' Skewness Exceptions
+#'
+#' @description
+#' `Skewness.Exceptions()` is an internal function that returns a set of exceptions used by `Skewness.Validation()` service.
+#' * The first exception: `argument.NULL`, indicates to the user that no sample data have to provided for analysis.
+#' * The second exception: `type.mismatch`, indicates to the user that the sample data provided is not numerical data.
+#' @usage NULL
+#' @returns A `list` of exceptions: 
+#' * `argument.NULL()`
+#' * `type.mismatch()`
+Skewness.Exceptions <- \() {
+  exceptions <- list()
+  exceptions[['argument.NULL']] <- \(invoke) {
+    if(invoke) stop("Argument.NULL: 'sample' cannot be NULL.", call. = FALSE)
+  }
+  exceptions[['type.mismatch']] <- \(invoke, actual = NULL) {
+    if (invoke) stop("Type.Mismatch: Got '", actual, "' but expected 'numeric'.", call. = FALSE)
+  }
+  return(exceptions)
+}
