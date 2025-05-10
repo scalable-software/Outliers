5fbb3da4ff21e3a707128e462d67fb5498447563
Architect <solution.architect@scalable.software>
2025-05-10 18:41:46 +0200

    EXCEPTIONS: Scaling.Exceptions -> FAIL

---
+++ tests/testthat/test-Scaling.Exceptions.R
@@ -0,0 +1,80 @@
+describe("Given Scaling.Exceptions",{
+  it("exist",{
+    Scaling.Exceptions |> expect.exist()
+  })
+})
+
+describe("When exceptions <- Scaling.Exceptions()",{
+  it("then exceptions is a list",{
+    # GIVEN
+    exceptions <- Scaling.Exceptions()
+
+    # THEN
+    exceptions |> expect.list()
+  })
+  it("then exceptions contains 'argument.NULL'",{
+    # GIVEN
+    exceptions <- Scaling.Exceptions()
+
+    # THEN
+    exceptions[['argument.NULL']] |> expect.exist()
+  })
+  it("then exceptions contains 'type.mismatch'",{
+    # GIVEN
+    exceptions <- Scaling.Exceptions()
+
+    # THEN
+    exceptions[['type.mismatch']] |> expect.exist()
+  })
+})
+
+describe("When input |> exception[['argument.NULL']]()",{
+  it("then no exception is thrown when input is FALSE",{
+    # GIVEN
+    exception <- Scaling.Exceptions()
+
+    # WHEN
+    input <- FALSE
+
+    # THEN
+    input |> exception[['argument.NULL']]() |> expect.no.error()
+  })
+  it("then an exception is thrown when input is TRUE",{
+    # GIVEN
+    exception <- Scaling.Exceptions()
+
+    # WHEN
+    input <- TRUE
+
+    # THEN
+    error.message <- "Argument.NULL: 'sample' cannot be NULL."
+
+    input |> exception[['argument.NULL']]() |> expect.error(error.message)
+  })
+})
+
+describe("When input |> exception[['type.mismatch']]()",{
+  it("then no exception is thrown when input is FALSE",{
+    # GIVEN
+    exception <- Scaling.Exceptions()
+
+    # WHEN
+    input <- FALSE
+
+    # THEN
+    input |> exception[['type.mismatch']]() |> expect.no.error()
+  })
+  it("then an exception is thrown when input is TRUE",{
+    # GIVEN
+    exception <- Scaling.Exceptions()
+
+    # WHEN
+    input <- TRUE
+    type  <- "character"
+
+    # THEN
+    error.message <- "Type.Mismatch: Got 'character' but expected 'numeric'."
+
+    input |> exception[['type.mismatch']](type) |> expect.error(error.message)
+  })
+})
