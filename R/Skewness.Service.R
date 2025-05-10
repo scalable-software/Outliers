d8a59345e4a5be3649bd5e2a02e90a1d128cf496
Architect <solution.architect@scalable.software>
2025-05-10 13:47:51 +0200

    SERVICE: Skewness.Service -> PASS

---
+++ R/Skewness.Service.R
@@ -0,0 +1,27 @@
+#' Skewness Utility Services
+#'
+#' @description
+#' `Skewness.Service()` returns a set of utility functions that can be used to determine the skewness present in the sample data.
+#' * The first function: `medcouple`, calculates and returns the medcouple of a sample data set.
+#' 
+#' @usage NULL
+#' @returns A `list` of utility functions: 
+#' * `medcouple(data)`
+#' @examples
+#' skewness <- Skewness.Service()
+#' 
+#' skewness.medcouple <-
+#'   1000 |> rnorm(10,5) |> skewness[['medcouple']]()
+#' 
+#' rm(skewness.medcouple)
+#' @export
+Skewness.Service <- \() {
+  validate <- Skewness.Validator()
+
+  services <- list()
+  services[['medcouple']] <- \(sample) {
+    sample |> validate[['sample']]()
+    sample |> robustbase::mc()
+  }
+  return(services)
+}
