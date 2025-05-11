6fb99aae548a7c81bd5714a9ad90b9f999cb5dc4
Architect <solution.architect@scalable.software>
2025-05-11 19:43:09 +0200

    BROKER: Outlier.Broker -> FAIL

---
+++ tests/testthat/test-Outlier.Broker.R
@@ -0,0 +1,67 @@
+describe("Given Outlier.Broker", {
+  it("exist",{
+    Outlier.Broker |> expect.exist()
+  })
+})
+
+describe("Given operations <- Outlier.Broker() |> Outlier.Service()",{
+  it("then operations is a list",{
+    # GIVEN
+    operations <- Outlier.Broker() |> Outlier.Service()
+
+    # THEN
+    operations |> expect.list()
+  })
+  it("then operations contains 'extract' service",{
+    # GIVEN
+    operations <- Outlier.Broker() |> Outlier.Service()
+
+    # THEN
+    operations[['extract']] |> expect.exist()
+  })
+  it("then operations contains 'remove' service",{
+    # GIVEN
+    operations <- Outlier.Broker() |> Outlier.Service()
+
+    # THEN
+    operations[['remove']] |> expect.exist()
+  })
+})
+
+describe("When input |> operation[['extract']]()",{
+  it("then the outlier in input should be returned",{
+    # GIVEN
+    boundary <- Boundary.Service()
+    operation  <- boundary |> Outlier.Broker()
+    input    <- 1000 |> rnorm(10,5)
+
+    # WHEN
+    boundary.upper <- input |> boundary[['upper']]()
+    boundary.lower <- input |> boundary[['lower']]()
+
+    expected.outliers <- input[input < boundary.lower | input > boundary.upper]
+    actual.outliers   <- input |> operation[['extract']]()
+
+    # THEN
+    actual.outliers |> expect.equal(expected.outliers)
+  })
+})
+
+describe("When input |> operation[['remove']]()",{
+  it("then the outlier in input should be removed",{
+    # GIVEN
+    boundary <- Boundary.Service()
+    operation <- boundary |> Outlier.Broker()
+    input    <- 1000 |> rnorm(10,5)
+
+    # WHEN
+    boundary.upper <- input |> boundary[['upper']]()
+    boundary.lower <- input |> boundary[['lower']]()
+
+    expected.outliers <- input[input >= boundary.lower & input <= boundary.upper]
+    actual.outliers   <- input |> operation[['remove']]()
+
+    # THEN
+    actual.outliers |> expect.equal(expected.outliers)
+  })
+})
