7f101ec4d0e2cffe2f629ab1edc2bd2be9eb9997
Architect <solution.architect@scalable.software>
2025-05-11 14:46:05 +0200

    SERVICE: Boundary.Service -> PASS

---
+++ R/Boundary.Service.R
@@ -0,0 +1,37 @@
+#' Boundary Service
+#'
+#' @description
+#' `Boundary.Service()` is a function that returns a set of services used to calculate the upper and lower boundaries for outlier detection.
+#' It uses the inter quartile range (IQR) method and the skewness of the data to determine the boundaries.
+#' 
+#' @usage NULL
+#' @returns A `list` of services:
+#' * `upper()`: Calculates the upper boundary for outlier detection.
+#' * `lower()`: Calculates the lower boundary for outlier detection.
+#' 
+#' @export
+Boundary.Service <- \() {
+  validate <- Boundary.Validator()
+
+  quartile <- Quartile.Service()
+  range    <- IQR.Service() |> IQR.Processor()
+  skewness <- Skewness.Service()
+  scale    <- Scaling.Service()
+
+  services <- list()
+  services[['upper']] <- \(sample) {
+    sample |> validate[['sample']]()
+
+    (sample |> quartile[['third']]()) + 1.5 * 
+    (sample |> range[['IQR']]()) * 
+    (sample |> skewness[['medcouple']]() |> scale[['upper']]())
+  }
+  services[['lower']] <- \(sample) {
+    sample |> validate[['sample']]()
+
+    (sample |> quartile[['first']]()) - 1.5 * 
+    (sample |> range[['IQR']]()) * 
+    (sample |> skewness[['medcouple']]() |> scale[['lower']]())
+  }
+  return(services)
+}
