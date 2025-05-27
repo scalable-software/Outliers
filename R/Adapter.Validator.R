8dfda544344572053c8cf531f849000628a958f8
Architect <solution.architect@scalable.software>
2025-05-27 13:04:17 +0200

    VALIDATION: Adapter.Validator() ReturnsListOfValidatorsContainingExistValidator -> PASS

--- R/Adapter.Validator.R
+++ R/Adapter.Validator.R
@@ -2,6 +2,7 @@ Adapter.Validator <- \() {
   exceptions <- Adapter.Exceptions()
 
   validators <- list()
+  validators[['exist']]           <- \() {}
   validators[['is.data.frame']]   <- \(input) {
     input |> is.data.frame() |> isFALSE() |> exceptions[['type.mismatch']](class(input), 'data.frame')
     return(input)
