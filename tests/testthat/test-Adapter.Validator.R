ba2ccdece58ab038da225fb9eb49da41e35520ee
Architect <solution.architect@scalable.software>
2025-05-27 12:07:23 +0200

    VALIDATION: input |> validate[['is.data.frame']]() ThrowsExceptionIfInputNotDataFrame -> FAIL

--- tests/testthat/test-Adapter.Validator.R
+++ tests/testthat/test-Adapter.Validator.R
@@ -20,3 +20,37 @@ describe("When validators <- Adapter.Validator()",{
     adapters[['is.data.frame']] |> expect.exist()
   })
 })
+
+describe("When input |> validate[['is.data.frame']]()",{
+  it("then an exception should be thrown when input is not a data.frame",{
+    # GIVEN
+    validate <- Adapter.Validator()
+
+    # WHEN
+    input <- ''
+
+    # THEN
+    error.massage <- "Type.Mismatch: Got 'character' but expected 'data.frame'."
+    input |> validate[['is.data.frame']]() |> expect.error(error.massage)
+  })
+  it("then no exception should be thrown when input is a data.frame",{
+    # GIVEN
+    validate <- Adapter.Validator()
+
+    # WHEN
+    input <- data.frame()
+
+    # THEN
+    input |> validate[['is.data.frame']]() |> expect.no.error()
+  })
+  it("then input should be returned if input is numeric",{
+    # GIVEN
+    validate <- Adapter.Validator()
+
+    # WHEN
+    input <- data.frame()
+
+    # THEN
+    input |> validate[['is.data.frame']]() |> expect.equal(input)
+  })
+})
