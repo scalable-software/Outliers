36c1f12490eeb276855dfa67b2f965a19dd6c080
Architect <solution.architect@scalable.software>
2025-05-27 12:55:50 +0200

    VALIDATION: input |> validate[['is.valid.column']](column) ThrowsColumInvalidExceptionIfColumnNotInInputDataFrame -> PASS

--- R/Adapter.Validator.R
+++ R/Adapter.Validator.R
@@ -2,10 +2,13 @@ Adapter.Validator <- \() {
   exceptions <- Adapter.Exceptions()
 
   validators <- list()
-  validators[['is.data.frame']] <- \(input) {
+  validators[['is.data.frame']]   <- \(input) {
     input |> is.data.frame() |> isFALSE() |> exceptions[['type.mismatch']](class(input), 'data.frame')
     return(input)
   }
-  validators[['is.valid.column']] <- \() {}
+  validators[['is.valid.column']] <- \(input, column) {
+    input[[column]] |> is.null() |> exceptions[['column.invalid']](column)
+    return(input)
+  }
   return(validators)
 }
