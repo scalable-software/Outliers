9e5f6a9d2f321b00811f98f11816f72ddcf4bcc7
Architect <solution.architect@scalable.software>
2025-05-27 12:07:28 +0200

    VALIDATION: input |> validate[['is.data.frame']]() ThrowsExceptionIfInputNotDataFrame -> PASS

--- R/Adapter.Validator.R
+++ R/Adapter.Validator.R
@@ -1,5 +1,10 @@
 Adapter.Validator <- \() {
+  exceptions <- Adapter.Exceptions()
+
   validators <- list()
-  validators[['is.data.frame']] <- \() {}
+  validators[['is.data.frame']] <- \(input) {
+    input |> is.data.frame() |> isFALSE() |> exceptions[['type.mismatch']](class(input), 'data.frame')
+    return(input)
+  }
   return(validators)
 }
