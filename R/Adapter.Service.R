582bbd98256a5a45487dbf14677dbe0c985fb2d7
Architect <solution.architect@scalable.software>
2025-05-27 13:19:14 +0200

    SERVICE: input |> service[['extract.sample']](column) ThrowsArgumentNULLExceptionIfInputNULL -> PASS

--- R/Adapter.Service.R
+++ R/Adapter.Service.R
@@ -1,6 +1,10 @@
 Adapter.Service <- \(broker) {
+  validate <- Adapter.Validator()
+
   services <- list()
   services[['extract.sample']] <- \(data, column) {
+    data |> validate[['exist']]()
+    
     data |> broker[['extract.sample']](column)
   }
   return(services)
