4c979a7c1262da8b955f63011fd43ffde0a135a5
Architect <solution.architect@scalable.software>
2025-05-27 11:47:52 +0200

    EXCEPTIONS: TRUE |> exception[['type.mismatch']](type) ThrowsTypeMismatchException -> FAIL

--- tests/testthat/test-Adapter.Exceptions.R
+++ tests/testthat/test-Adapter.Exceptions.R
@@ -20,3 +20,29 @@ describe("Given exceptions <- Adapter.Exceptions()", {
     exceptions[['type.mismatch']] |> expect.exist()
   })
 })
+
+describe("When input |> exception[['type.mismatch']]()",{
+  it("then no exception is thrown when input is FALSE",{
+    # GIVEN
+    exception <- Adapter.Exceptions()
+
+    # WHEN
+    input <- FALSE
+
+    # THEN
+    input |> exception[['type.mismatch']]() |> expect.no.error()
+  })
+  it("then an exception is thrown when input is TRUE",{
+    # GIVEN
+    exception <- Adapter.Exceptions()
+
+    # WHEN
+    input <- TRUE
+    type  <- "character"
+
+    # THEN
+    error.message <- "Type.Mismatch: Got 'character' but expected 'numeric'."
+
+    input |> exception[['type.mismatch']](type) |> expect.error(error.message)
+  })
+})
