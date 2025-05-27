db2a8e5d5f0d60097fccdbd82327057b650f15fd
Architect <solution.architect@scalable.software>
2025-05-27 11:36:31 +0200

    BROKER: Adapter.Broker() ReturnsListContainingExtractSampleAdaptation -> FAIL

--- tests/testthat/test-Adapter.Broker.R
+++ tests/testthat/test-Adapter.Broker.R
@@ -12,4 +12,11 @@ describe("Given adaptations <- Adapter.Broker()",{
     # THEN
     adaptations |> expect.list()
   })
+   it("then adaptations contain 'extract.sample' adaptation",{
+    # GIVEN
+    adaptations <- Adapter.Broker()
+
+    # THEN
+    adaptations[['extract.sample']] |> expect.exist()
+  })
 })
