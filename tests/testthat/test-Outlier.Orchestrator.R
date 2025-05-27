1668d4afe85efe71070b0ece13778349d1b5d82c
Architect <solution.architect@scalable.software>
2025-05-27 14:42:50 +0200

    ORCHESTRATOR: input |> outliers[['extract']](column) ReturnsOutliersInColumnInDataFrame -> FAIL

--- tests/testthat/test-Outlier.Orchestrator.R
+++ tests/testthat/test-Outlier.Orchestrator.R
@@ -30,7 +30,7 @@ describe("When orchestrations <- Outlier.Orchestrator()",{
 
 
 describe("When input |> orchestration[['extract']]()",{
-  it("then the outlier in input should be returned",{
+  it("then the outlier in input should be returned from numeric vector",{
     # Given
     service <- Boundary.Broker() |> Boundary.Service()
 
@@ -43,6 +43,35 @@ describe("When input |> orchestration[['extract']]()",{
 
     expected.outliers <- input[input < boundary.lower | input > boundary.upper]
     actual.outliers   <- input |> orchestration[['extract']]()
+
+    # Then
+    actual.outliers |> expect.equal(expected.outliers)
+  })
+  it("then the outlier in input should be returned from a data.frame",{
+    # Given
+    service <- 
+      Boundary.Broker()  |>
+      Boundary.Service() |> 
+      Outlier.Broker()   |>
+      Outlier.Service()
+
+
+    adapter <-
+      Adapter.Broker() |> 
+      Adapter.Service()
+
+    orchestration <- Outlier.Orchestrator()
+
+    column <- 'value'
+
+    input <- 1000 |> rnorm(10,5) |> data.frame() |> setNames(column)
+
+    expected.outliers <- input[input |> 
+      adapter[['extract.sample']](column) |> 
+      service[['extract']](),]
+
+    # When
+    actual.outliers <- input |> orchestration[['extract']](column)
 
     # Then
     actual.outliers |> expect.equal(expected.outliers)
