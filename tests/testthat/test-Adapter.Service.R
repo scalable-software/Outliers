a1167f7fabbcb4606beb7b145d68e73c72e756a4
Architect <solution.architect@scalable.software>
2025-05-27 13:15:43 +0200

    SERVICE: input |> service[['extract.sample']](column) ReturnsInputSubSettingColumn -> FAIL

--- tests/testthat/test-Adapter.Service.R
+++ tests/testthat/test-Adapter.Service.R
@@ -20,3 +20,20 @@ describe("Given services <- Adapter.Service()",{
     services[['extract.sample']] |> expect.exist()
   })
 })
+
+describe("Given data |> service[['extract.sample']](column)",{
+  it("then sample data is returned based on specified column in data data.frame",{
+    # GIVEN
+    service <- Adapter.Broker() |> Adapter.Service()
+
+    # WHEN
+    data   <- data.frame(a = 1:5, b = 6:10)
+    column <- 'a'
+
+    actual <- data |> service[['extract.sample']](column)
+
+    # THEN
+    expected <- data[[column]]
+    actual |> expect.equal(expected)
+  })
+})
